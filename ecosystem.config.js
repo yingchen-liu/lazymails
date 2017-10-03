a3e535f9d2d324774455fdac255e654c085a2893
xiuxiu <ytxiuxiu@gmail.com>
2017-10-03 16:12:25 +1100

    Fixed deploy subdir npm install issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:ytxiuxiu/fit5140-lazy-mail.git',
       path : '/lazymails',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install --prefix server && pm2 reload ecosystem.config.js --env production'
     }
   }
 };

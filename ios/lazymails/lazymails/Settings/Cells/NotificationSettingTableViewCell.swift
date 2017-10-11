c39d0254093270412f9d198f099084d0c7b4ecb8
xiuxiu <ytxiuxiu@gmail.com>
2017-10-11 12:47:53 +1100

    Added energy saving, notification, receiver setting page

---
+++ ios/lazymails/lazymails/Settings/Cells/NotificationSettingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationSettingTableViewCell.swift
+//  lazymails
+//
+//  Created by YINGCHEN LIU on 11/10/17.
+//  Copyright © 2017 YINGCHEN LIU. All rights reserved.
+//
+
+import UIKit
+
+class NotificationSettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ios/lazymails/lazymails/Settings/Cells/ReceiverSettingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceiverSettingTableViewCell.swift
+//  lazymails
+//
+//  Created by YINGCHEN LIU on 11/10/17.
+//  Copyright © 2017 YINGCHEN LIU. All rights reserved.
+//
+
+import UIKit
+
+class ReceiverSettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

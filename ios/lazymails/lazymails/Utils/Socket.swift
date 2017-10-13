5ee630ff08b553e1959c4adffd76c97b2248245d
xiuxiu <ytxiuxiu@gmail.com>
2017-10-13 16:34:35 +1100

    Settings

---
+++ ios/lazymails/lazymails/Utils/Socket.swift
@@ -0,0 +1,15 @@
+//
+//  Socket.swift
+//  lazymails
+//
+//  Created by YINGCHEN LIU on 12/10/17.
+//  Copyright © 2017 YINGCHEN LIU. All rights reserved.
+//
+
+import UIKit
+
+class Socket: NSObject {
+    
+    
+
+}
